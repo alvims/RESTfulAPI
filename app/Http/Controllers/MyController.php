324eacc6e9d1bb81ce0797872755d10060c57324
alvims <alvims@gmail.com>
2016-10-30 07:43:39 -0400

    primer commit
    -ceracion laravel

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,17 @@
+<?php namespace App\Http\Controllers;
+
+use App\Prueba;
+
+class MyController extends Controller
+{
+ public function index()
+ {
+ 	$modelo = new Prueba();
+
+ 	$saludo = $modelo->saludar("Pedro");
+
+ 	return view('prueba.index',['saludo'=> $saludo]);
+ }
+}
+
+?>
